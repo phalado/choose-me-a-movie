28c260c96ee160a77bfaef67caf487293fb09901
Rapha <phalado@gmail.com>
2023-08-26 21:37:41 -0300

    Choose a Movie - Hello World

---
+++ src/components/screens/InitialScreen.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from 'react-native';
+import styles from '../styles/InitialScreen'
+
+const InitialScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Choose a movie - Hello World!</Text>
+    </View>
+  );
+}
+
+export default InitialScreen;
---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import InitialScreen from './components/screens/InitialScreen';
+
+const App = () => (
+  <InitialScreen />
+)
+
+export default App;
